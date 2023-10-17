cb2a76318dcb8eaf580c5d471d64be915bff4294
FedeGalli <federicogalli96gmail.com>
2023-10-17 21:02:13 +0200

    splitting the app in modules

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {View, Text, SafeAreaView, StyleSheet} from 'react-native'
-import CurrentWeather from "./src/components/CurrentWeather";
+
+import UpcomingWeather from "./src/components/UpcomingWeather";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <CurrentWeather />
+      <UpcomingWeather />
     </View>
   )
 }
@@ -13,7 +14,8 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container:{
-    flex: 1
+    flex: 1,
+    paddingTop: 40,
   }
 })
 export default App
