7722a1af0576856859d7549233dd4ede70f5236c
FedeGalli <federicogalli96@gmail.com>
2023-10-20 17:38:46 +0200

    hooks and loading screen

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { NavigationContainer} from '@react-navigation/native'
+import { StyleSheet, View } from "react-native";
 import Tabs from './src/components/Tabs'
+import { ActivityIndicator } from "react-native";
+
+
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect( () => {
+    (async() => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted'){
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync()
+      setLocation(location)
+    })()
+  }, [])
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'green'}></ActivityIndicator>
+      </View>
+    )
+  }
+
+
+
+  if (location) {
+    console.log(location)
+  }
   return (
     <NavigationContainer>
       <Tabs></Tabs>
@@ -10,5 +43,13 @@ const App = () => {
 
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: 'pink'
+  }
+})
 
 export default App
